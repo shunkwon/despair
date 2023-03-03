1ddd6501fd6a0595f4fe0be647c4a89feba1d394
gy <email1805772754@qq.com>
2023-03-03 23:57:14 +0800

    fix

--- projects/0x5617/index.js
+++ projects/0x5617/index.js
@@ -1,47 +1,88 @@
 const fs = require("fs")
 
-let data = fs.readFileSync("./test.txt").toString()
 
 
-let handledData = data.split(/(?<=\})/).map(v => JSON.parse(v))
-let o = []
+function main() {
+    // 读取文件
+    let data = readPrimitiveFile("./test.txt")
+        // 文件转化
+    let handledData = getHandledData(data)
+        // 对数据进行去重
+    let cleanedHandleData = getCleanedHandleData(handledData)
+        // 最终处理
+    let finnalHandleData = getLastHandleData(cleanedHandleData)
 
-for (let j = handledData.length - 2; j >= 0; j--) {
-    if (handledData[j].tokenBalance !== handledData[j + 1].tokenBalance) {
-        o.unshift(handledData[j])
+    // log out
+    // logData(finnalHandleData)
+
+    // 盈亏平衡计算
+    let result = getPNL(finnalHandleData)
+
+    console.log(result)
+}
+
+function readPrimitiveFile(path) {
+    return fs.readFileSync(path).toString()
+}
+
+function getHandledData(data) {
+    return data.split(/(?<=\})/).map(v => JSON.parse(v))
+}
+
+function getCleanedHandleData(data) {
+    let o = []
+
+    let left = 0
+    let right = 1
+    while (right < data.length) {
+        o.push(data[left])
+        while (right < data.length && data[right].tokenBalance === data[left].tokenBalance) right++;
+        left = right
+    }
+    return o
+}
+
+function getLastHandleData(data) {
+    let cha = []
+    for (let i = 1; i < data.length; i++) {
+
+        cha.push({
+            pre: {
+                year: data[i - 1].year,
+                month: data[i - 1].month,
+                day: data[i - 1].day,
+                hour: data[i - 1].hour,
+                minute: data[i - 1].minute
+            },
+            cur: {
+                year: data[i].year,
+                month: data[i].month,
+                day: data[i].day,
+                hour: data[i].hour,
+                minute: data[i].minute
+            },
+            balanceDiff: data[i].tokenBalance - data[i - 1].tokenBalance,
+            ethusd: data[i - 1].ethusd,
+        })
+    }
+    return cha
+}
+
+
+function logData(data) {
+    for (let item of data) {
+        console.log(JSON.stringify(item, ["balanceDiff", "ethusd", "pre", "cur", "year", "month", "day", "hour", "minute"], 4))
     }
 }
 
-let cha = []
-for (let i = 1; i < o.length; i++) {
+function getPNL(data) {
+    let res = 0
 
-    cha.push({
-        pre: {
-            year: o[i - 1].year,
-            month: o[i - 1].month,
-            day: o[i - 1].day,
-            hour: o[i - 1].hour,
-            minute: o[i - 1].minute
-        },
-        cur: {
-            year: o[i].year,
-            month: o[i].month,
-            day: o[i].day,
-            hour: o[i].hour,
-            minute: o[i].minute
-        },
-        tokenBalance: o[i].tokenBalance - o[i - 1].tokenBalance,
-        ethusd: o[i].ethusd - o[i - 1].ethusd,
-    })
-}
-
-for (let item of cha) {
-    console.log(JSON.stringify(item, ["tokenBalance", "ethusd", "pre", "cur", "year", "month", "day", "hour", "minute"], 4))
+    for (let m = 1; m < data.length; m++) {
+        res += (data[m].ethusd - data[m - 1].ethusd) * data[m].balanceDiff
+    }
+    return res
 }
 
 
-// fs.writeFileSync("./data.json", JSON.stringify(cha))
-let res = cha.reduce((pre, cur) => {
-    let { tokenBalance, ethusd } = cur
-    return pre + tokenBalance * Math.abs(ethusd)
-}, 0)
+main()
